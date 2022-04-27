d758dc323960323a172f420b897e28574d487bd0
Ronaldo Pires Dias <ronaldo.pires@armac.com.br>
2022-04-27 18:46:51 -0300

    cypress

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -1,8 +1,24 @@
 describe("Testando Formulario", () => {
     it("Preenchendo o formulário", () => {
         //Acessando a URL base 
-    cy.visit("/");
-    //Selecionando o FORM
-    cy.get("form");
-      });
+        cy.visit("/");
+        //Selecionando o FORM
+        cy.get("form");
+
+        cy.get('input[name="name"]')
+            .type("Isabella")
+            .should("have.value", "Isabella");
+
+        cy.get('input[name="email"]')
+            .type("isa@dev.com")
+            .should("have.value", "isa@dev.com");
+
+        cy.get("textarea")
+            .type("Esta é a mensagem a ser enviada!")
+            .should("have.value", "Esta é a mensagem a ser enviada!");
+        
+            cy.get("form").submit();
+
+
+    });
 });
