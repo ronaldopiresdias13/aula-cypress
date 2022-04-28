72c38b3d148b9c70b8f6d9b70a44d5de29824835
Ronaldo Pires Dias <ronaldo.pires@armac.com.br>
2022-04-27 22:16:51 -0300

    cypress

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -6,18 +6,24 @@ describe("Testando Formulario", () => {
         cy.get("form");
 
         cy.get('input[name="name"]')
-            .type("Isabella")
-            .should("have.value", "Isabella");
+            .type("Ronaldo")
+            .should("have.value", "Ronaldo");
 
         cy.get('input[name="email"]')
-            .type("isa@dev.com")
-            .should("have.value", "isa@dev.com");
+            .type("ronaldo@dev.com")
+            .should("have.value", "ronaldo@dev.com");
 
         cy.get("textarea")
             .type("Esta é a mensagem a ser enviada!")
             .should("have.value", "Esta é a mensagem a ser enviada!");
-        
-            cy.get("form").submit();
+
+        cy.server();
+        cy.route({
+          url: "/users/**",
+          method: "POST",
+          response: { status: "Saved", code: 201 }
+        });
+        cy.get("form").submit();
 
 
     });
--- form.js
+++ form.js
@@ -2,4 +2,18 @@ const form = document.forms[0];
 
 form.addEventListener("submit", event => {
   event.preventDefault();
+  new FormData(form);
+});
+
+document.addEventListener("formdata", event => {
+  const body = Object.fromEntries(event.formData.entries());
+  const jsonBody = JSON.stringify(body);
+  const request = new XMLHttpRequest();
+  request.open("POST", "https://jsonplaceholder.typicode.com/users/");
+  request.send(jsonBody);
+  // Obtendo a resposta JSON
+  request.onload = function() {
+    const jsonResponse = JSON.parse(this.response);
+    document.body.innerHTML += `Resposta vinda do servidor: ${jsonResponse.status}`;
+  };
 });
